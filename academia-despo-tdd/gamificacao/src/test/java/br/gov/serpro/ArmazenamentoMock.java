9cf90f0b7e94ff944de299d5abc74eef5f3e7bc5
27352403809 <davison.duraes@gmail.com>
2019-06-11 16:55:14 -0300

    implementação incompleta 1º cenario

--- academia-despo-tdd/gamificacao/src/test/java/br/gov/serpro/ArmazenamentoMock.java
+++ academia-despo-tdd/gamificacao/src/test/java/br/gov/serpro/ArmazenamentoMock.java
@@ -1,14 +1,22 @@
 package br.gov.serpro;
 
-import java.util.List;
+import java.util.Map;
 
 /**
  * Created by 27352403809 on 10/06/19.
  */
 public class ArmazenamentoMock implements Armazenamento{
 
-    public ArmazenamentoMock(String nomeCompetidor, TipoPonto tipoPonto, int pontos){
+    private Map<TipoPonto, Integer> score;
+    private String nomeCompetidor;
 
+    public ArmazenamentoMock(String nomeCompetidor, TipoPonto tipoPonto, Integer pontos){
+        score.put(tipoPonto, pontos);
+        this.nomeCompetidor = nomeCompetidor;
+    }
+
+    public String computarScore(){
+        return "O usuário " + nomeCompetidor + " recebeu " + score.entrySet().toString() + " pontos do tipo estrela";
     }
 
 }
